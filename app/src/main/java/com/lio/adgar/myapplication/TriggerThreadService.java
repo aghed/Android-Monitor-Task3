fb9a220f7f9cd6a229eeae5cd1016523e4c75d33
aghed <aghed@local.com>
2017-08-15 14:59:03 +0300

    a small code to track connection wheter it is wifi or mobile data

--- app/src/main/java/com/lio/adgar/myapplication/TriggerThreadService.java
+++ app/src/main/java/com/lio/adgar/myapplication/TriggerThreadService.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
  */
 public class TriggerThreadService extends Service{
     public static final String MESSAGE_TAG="note";
+    public String CONNECTION_TYPE;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,13 +28,25 @@ public class TriggerThreadService extends Service{
             public void run() {
                 //check for Connectivity
                 while ( NetWorkUtil.getConnectivityStatus(getApplicationContext())!= NetWorkUtil.TYPE_NOT_CONNECTED ) {
-                    
+
+                    if(NetWorkUtil.getConnectivityStatus(getApplication())==NetWorkUtil.TYPE_WIFI)
+                        CONNECTION_TYPE="Wi fi";
+
+                    else
+                    if(NetWorkUtil.getConnectivityStatus(getApplicationContext())==NetWorkUtil.TYPE_MOBILE_DATA)
+
+                        CONNECTION_TYPE="Mobile Data";
+                    if(! CONNECTION_TYPE.isEmpty())
+                        Log.d(MESSAGE_TAG,"Connection Type : "+CONNECTION_TYPE);
+                    else
+                    //it is connected but not through wifi or mobile data then we don't want it
+                    this.interrupt();
                     try {
-                        //make any task you want every 5 minutes 'I made it only 5 sec to see the results faster instead of waiting 30 seconds'
+                        //make any task you want every 5 seconds 'I made it only 5 sec to see the results faster instead of waiting 30 seconds'
                         Thread.sleep(5000);
                         Log.d(MESSAGE_TAG,"thread running");
                     } catch (InterruptedException e) {
-                        Log.d("Exception","thread faild to sleep");
+                        Log.d(MESSAGE_TAG,"thread is killed");
                     }
                 }
                 //if no Connection is Active Kill the thread
